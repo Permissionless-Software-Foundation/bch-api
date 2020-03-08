4c4294001f4c66017ae5e0b32f83d060802c186d
Chris Troutner <chris.troutner@gmail.com>
2020-03-08 03:58:51 +0000

    Updating testnet docker-compose file to use localized redis

--- docker/mainnet/start-local-mainnet.sh
+++ docker/mainnet/start-local-mainnet.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
 # Full node
-export RPC_BASEURL=http://172.17.0.1:8332/
+export RPC_BASEURL=http://142.93.13.2:8332/
 export RPC_USERNAME=bitcoin
 export RPC_PASSWORD=password
 export NETWORK=mainnet
 
 # SLPDB
 #export SLPDB_URL=https://slpdb.bitcoin.com/
-export SLPDB_URL=http://172.17.0.1:12300/
+#export SLPDB_URL=http://172.17.0.1:12300/
+export SLPDB_URL=https://slpdb2.bchtest.net/
+export SLPDB_PASS=owmvgnsksoapwhrnvu
 
 # Blockbook Indexer
-export BLOCKBOOK_URL=https://172.17.0.1:9131/
+export BLOCKBOOK_URL=https://157.230.214.175:9131/
 # Allow node.js to make network calls to https using self-signed certificate.
 export NODE_TLS_REJECT_UNAUTHORIZED=0
 
-export JWT_AUTH_SERVER=http://localhost:5001/
+export JWT_AUTH_SERVER=http://172.17.0.1:5001/
 
 # Redis DB
 export REDIS_PORT=6379
--- docker/testnet/start-local-testnet.sh
+++ docker/testnet/start-local-testnet.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # Full node
-export RPC_BASEURL=http://172.17.0.1:18332/
+export RPC_BASEURL=http://206.189.188.192:18332/
 export RPC_USERNAME=bitcoin
 export RPC_PASSWORD=password
-export ZEROMQ_PORT=38332
-export ZEROMQ_URL=172.17.0.1
 export NETWORK=testnet
 
 # SLPDB
-#export SLPDB_URL=https://tslpdb.bitcoin.com/
-export SLPDB_URL=http://172.17.0.1:13300/
+export SLPDB_URL=https://tslpdb.bitcoin.com/
+#export SLPDB_URL=http://172.17.0.1:12300/
 
-# Blockbook
-export BLOCKBOOK_URL=https://172.17.0.1:19131/
+# Blockbook Indexer
+export BLOCKBOOK_URL=https://157.230.178.198:19131/
 # Allow node.js to make network calls to https using self-signed certificate.
 export NODE_TLS_REJECT_UNAUTHORIZED=0
+
+export JWT_AUTH_SERVER=http://172.17.0.1:5001/
 
 # Redis DB
 export REDIS_PORT=6380
