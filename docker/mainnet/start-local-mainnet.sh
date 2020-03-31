94e4fbe9d4e0461b6c601dbfb9893905c739480a
Chris Troutner <chris.troutner@gmail.com>
2020-03-30 18:56:49 -0700

    Updating docker scripts

--- docker/mainnet/start-local-mainnet.sh
+++ docker/mainnet/start-local-mainnet.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
+# test
+
 # Full node
-export RPC_BASEURL=http://142.93.13.2:8332/
+export RPC_BASEURL=http://172.17.0.1:8332/
 export RPC_USERNAME=bitcoin
 export RPC_PASSWORD=password
 export NETWORK=mainnet
 
 # SLPDB
-#export SLPDB_URL=https://slpdb.bitcoin.com/
-#export SLPDB_URL=http://172.17.0.1:12300/
-export SLPDB_URL=https://slpdb2.bchtest.net/
-export SLPDB_PASS=owmvgnsksoapwhrnvu
+export SLPDB_URL=http://172.17.0.1:12300/
+export SLPDB_PASS=somelongpassword
 
 # Blockbook Indexer
-export BLOCKBOOK_URL=https://157.230.214.175:9131/
+export BLOCKBOOK_URL=https://172.17.0.1:9131/
 # Allow node.js to make network calls to https using self-signed certificate.
 export NODE_TLS_REJECT_UNAUTHORIZED=0
 
--- docker/testnet/start-local-testnet.sh
+++ docker/testnet/start-local-testnet.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 # Full node
-export RPC_BASEURL=http://206.189.188.192:18332/
+export RPC_BASEURL=http://172.17.0.1:18332/
 export RPC_USERNAME=bitcoin
 export RPC_PASSWORD=password
 export NETWORK=testnet
 
 # SLPDB
-export SLPDB_URL=https://tslpdb.bitcoin.com/
-#export SLPDB_URL=http://172.17.0.1:12300/
+export SLPDB_URL=http://172.17.0.1:13300/
 
 # Blockbook Indexer
-export BLOCKBOOK_URL=https://157.230.178.198:19131/
+export BLOCKBOOK_URL=https://172.17.0.1:19131/
 # Allow node.js to make network calls to https using self-signed certificate.
 export NODE_TLS_REJECT_UNAUTHORIZED=0
 
