6fff935248a1567c3246aab96f64bcc0e3ba3aa9
Chris Troutner <chris.troutner@gmail.com>
2020-04-04 20:33:05 -0700

    Writing debugging to the logger

--- src/middleware/route-ratelimit.js
+++ src/middleware/route-ratelimit.js
@@ -77,7 +77,7 @@ class RateLimits {
         //   jwtOptions
         // )
 
-        console.log(`Last three letters of token secret: ${config.apiTokenSecret.slice(-3)}`)
+        wlogger.info(`Last three letters of token secret: ${config.apiTokenSecret.slice(-3)}`)
         decoded = _this.jwt.verify(req.locals.jwtToken, config.apiTokenSecret)
         // console.log(`decoded: ${JSON.stringify(decoded, null, 2)}`)
 
