2172d0feff3dc27ca0270ce7bd83a04dd0cc6025
Chris Troutner <chris.troutner@gmail.com>
2021-01-11 15:33:45 -0800

    Updating example shell script

--- start-dev-example.sh
+++ start-dev-example.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-# Full node
-export RPC_BASEURL=http://<full node ip>:8332/
-export RPC_USERNAME=<RPC username>
-export RPC_PASSWORD=<RPC password>
+# Which network are you using?
 export NETWORK=mainnet
+#export NETWORK=testnet
+
+# Full node
+export RPC_IP=<ip>:8332
+export RPC_BASEURL=http://$RPC_IP/
+export RPC_USERNAME=bitcoin
+export RPC_PASSWORD=password
 
 # SLPDB
 export SLPDB_URL=http://<SLPDB IP>:12300/
 export SLPDB_PASS=somelongpassword
-
 # Use the same address as SLPDB_URL if you don't have a separate whitelist server.
 export SLPDB_WHITELIST_URL=http://<SLPDB IP>:12300/
 
@@ -17,15 +20,13 @@ export SLPDB_WHITELIST_URL=http://<SLPDB IP>:12300/
 # https://github.com/Permissionless-Software-Foundation/slp-api
 export SLP_API_URL=http://10.0.0.5:5001/
 
-# Blockbook
-export BLOCKBOOK_URL=https://<Blockbook IP>:9131/
-# Allow node.js to make network calls to https using self-signed certificate.
-export NODE_TLS_REJECT_UNAUTHORIZED=0
-
 # Mainnet Fulcrum / ElectrumX
 export FULCRUM_URL=192.168.0.6
 export FULCRUM_PORT=50002
 
+# JWT Token Secret
+# This is used to verify JWT tokens generated with jwt-bch-api:
+# https://github.com/Permissionless-Software-Foundation/jwt-bch-api
 export TOKENSECRET=somelongpassword
 
 # So that bch-api can call bch-js locally.
@@ -33,5 +34,16 @@ export LOCAL_RESTURL=http://127.0.0.1:3000/v3/
 
 # Basic Authentication password
 export PRO_PASS=somerandomepassword:someotherrandompassword:aThirdPassword
+
+# Whitelisted domains. Automatically give pro-tier rate limit access to apps
+# that originate froma domain on the whitelist.
+export WHITELIST_DOMAINS=fullstack.cash,psfoundation.cash,torlist.cash
+
+# Rate Limits. Numbers are divided into 1000. e.g. 1000 / 50 = 20 RPM for ANON.
+# Requests use the ANON rate limit if they fail to pass in a JWT token.
+# ANON = 20 requests per minute (RPM)
+export ANON_RATE_LIMIT=50
+# 10 = 100 RPM
+export WHITELIST_RATE_LIMIT=10
 
 npm start
