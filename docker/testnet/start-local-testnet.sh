7c177f801357fc76abe29d9869aa36e005343db3
Chris Troutner <chris.troutner@gmail.com>
2021-10-27 08:31:03 -0700

    Updating docker startup script

--- docker/testnet/start-local-testnet.sh
+++ docker/testnet/start-local-testnet.sh
@@ -17,29 +17,32 @@ export RPC_PASSWORD=password
 
 # SLPDB
 export SLPDB_URL=http://172.17.0.1:13300/
-export SLPDB_PASS=somelongpassword
+export SLPDB_PASS=portlandisacityinoregon
+export SLPDB_PASS_GP=portlandisacityinoregon
+export SLPDB_PASS_WL=portlandisacityinoregon
 # Use the same address as SLPDB_URL if you don't have a separate whitelist server.
 export SLPDB_WHITELIST_URL=http://172.17.0.1:13300/
 # slp-api alternative SLP validator using slp-validate:
 # https://github.com/Permissionless-Software-Foundation/slp-api
-export SLP_API_URL=http://10.0.0.5:5001/
+export SLP_API_URL=https://slpapi-testnet3.fullstackslp.nl/
 
 # Mainnet Fulcrum / ElectrumX
 export FULCRUM_URL=172.17.0.1
 export FULCRUM_PORT=60002
+export FULCRUM_API=http://172.17.0.1:3000/v1/
 
 # Redis DB
 export REDIS_PORT=6380
 export REDIS_HOST=172.17.0.1
 
 # JWT Token Secret
-export TOKENSECRET=somelongsecretvalue
+export TOKENSECRET=fridayharbor
 
 # So that bch-api can call bch-js locally.
 export LOCAL_RESTURL=http://127.0.0.1:3000/v4/
 
 # Basic Authentication password (optional)
-export PRO_PASS=somerandomepassword:someotherrandompassword:aThirdPassword
+export PRO_PASS=thejaasjhehrakseuhakuhr
 
 # Whitelisted domains. Automatically give pro-tier rate limit access to apps
 # that originate froma domain on the whitelist.
@@ -50,6 +53,9 @@ export WHITELIST_DOMAINS=fullstack.cash,psfoundation.cash,torlist.cash
 # ANON = 20 requests per minute (RPM)
 export ANON_RATE_LIMIT=50
 # 10 = 100 RPM
-export WHITELIST_RATE_LIMIT=10
+export WHITELIST_RATE_LIMIT=1
+
+export INTERNAL_RATE_LIMIT=100
 
 npm start
+
